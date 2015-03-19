22f094dc16e00ad40f11e9b76df5c9e8d026f273
jiri holub <jiri.holubcz@gmail.com>
2015-03-19 16:08:21 +0100

    first commit

---
+++ js/fc.js
@@ -0,0 +1,121 @@
+
+function factorial(num) {
+    // If the number is less than 0, reject it.
+    if (num < 0) {
+        return -1;
+    }
+    // If the number is 0, its factorial is 1.
+    else if (num == 0) {
+        return 1;
+    }
+    // Otherwise, call this recursive procedure again.
+    else {
+        return (num * factorial(num - 1));
+    }
+}
+
+function well_fce(u) {
+
+    suma = 0;
+
+    for (n = 2; n < 20; n++) {
+
+        if (n % 2 == 0) {
+            suma = suma - (Math.pow(u, n) / (factorial(n) * n));
+        } else {
+            suma = suma + (Math.pow(u, n) / (factorial(n) * n));
+        }
+    }
+
+    W = -0.577216 - Math.log(u) + u + suma;
+
+    return W;
+
+}
+
+function TheisDrawDown() {
+
+    r = 100;
+    S = $("#Storativity").val();
+    T = $("#Transmissivity").val(); // m2/min
+    Q = $("#Recharge").val();  // m3/min
+
+    t = new Array(60, 300, 600, 3000, 6000, 30000, 60000, 300000, 600000, 3000000);
+
+    $("#resultTabBody").empty();
+
+    for (i = 0; i < t.length; i++) {
+
+        u = ((r * r) * S) / (4 * T * t[i]);
+
+        s = (Q / (4 * Math.PI * T)) * well_fce(u);
+
+        $("#resultTabBody").append("<tr><td>" + Number((t[i] / 60).toFixed(1)) + "</td><td>" + Number((s).toFixed(3)) + "</td></tr>");
+    }
+}
+
+function TheisFce(t, r) {
+
+    S = $("#Storativity").val();
+    T = $("#Transmissivity").val(); // m2/min
+    Q = $("#Recharge").val();  // m3/min
+
+    u = ((r * r) * S) / (4 * T * t);
+
+    s = (Q / (4 * Math.PI * T)) * well_fce(u);
+
+    return s;
+}
+
+function drawPlot() {
+    data = new Array();
+    i = 0;
+    t = $("#time").val() * 60;
+    stopSolving = false;
+
+    for (r = -1; r > -1001; r--) {
+        if (stopSolving == false) {
+            s = TheisFce(t, r) * (-1);
+            data[i] = [r, s];
+        } else {
+            data[i] = [r, 0]
+        }
+
+        if (Math.abs(s) < 0.0001) {
+            stopSolving = true;
+        }
+        i++;
+    }
+
+    stopSolving = false;
+    for (r = 1; r < 1001; r++) {
+        if (stopSolving == false) {
+            s = TheisFce(t, r) * (-1);
+            data[i] = [r, s];
+        } else {
+            data[i] = [r, 0]
+        }
+
+        if (Math.abs(s) < 0.0001) {
+            stopSolving = true;
+        }
+        i++;
+    }
+    ploting(data, 'graph');
+}
+
+function animation() {
+
+    end = parseInt($("#end").val());
+    step = parseInt($("#step").val());
+
+        setTimeout(function() {
+            $("#time").val(parseInt($("#time").val()) + step);
+            drawPlot();
+            if(parseInt($("#time").val()) + step < end) {
+                animation();    
+            };
+        }, 200);
+        
+    
+}
---
+++ js/ploting.js
@@ -0,0 +1,69 @@
+
+function ploting(data, id) {
+
+    document.getElementById(id).innerHTML = "";
+
+    $.jqplot(id, [data], {
+        title: 'Drawdown in time',
+        grid: {
+            drawGridLines: true,
+            backgroundColor: "#F7FCFD",
+            gridLineColor: '#E8E8E8',
+            gridLineWidth: 1,
+            borderWidth: 0.1,
+            shadow: false
+        },
+        seriesDefaults: {
+            markerOptions: {
+                size: 7,
+                shadow: false
+            },
+            shadow: false
+        },
+        highlighter: {
+            sizeAdjust: 10,
+            tooltipLocation: 'n',
+            tooltipAxes: 'y',
+            useAxesFormatters: false,
+            showTooltip: true,
+            tooltipFade: true
+        },
+        cursor: {
+            show: true,
+            tooltipLocation: 'se',
+            zoom: false,
+            dblClickReset: true,
+            showVerticalLine: true,
+            showHorizontalLine: true,
+            fontSize: '13px'
+        },
+        legend: {
+            show: true,
+            location: 'en'
+        },
+        series: [
+            {label: 'Pumping test', isDragable: false, color: "blue", lineWidth: 2, markerOptions: {size: 3, shadow: false}, shadow: false},
+        ],
+        axes: {
+            xaxis: {
+                label: "r [m]",
+                //ticks: [0.01,0.1,1, 10, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1000000000],
+                tickOptions: {
+                 //   formatString: '%.1e'                           //http://perldoc.perl.org/functions/sprintf.html                      
+                },
+                //renderer: $.jqplot.LogAxisRenderer
+            },
+            yaxis: {
+                min: -5,
+                max: 1,
+                ticks: [-5,-4,-3,-2,-1,0,1], 
+                label: "s [m]",
+                tickOptions: {
+                    fontSize: '12px',
+                   // formatString: '%.1f'                           //http://perldoc.perl.org/functions/sprintf.html                      
+                }
+            }
+        }
+    });
+}
+
